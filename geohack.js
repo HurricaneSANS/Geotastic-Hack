f207a29a108a75295c7ef52f2003a0084571ae21
PolytoriaMan <125764413+HurricaneSANS@users.noreply.github.com>
2023-05-02 11:51:17 -0600

    Update geohack.js

--- geohack.js
+++ geohack.js
@@ -1,16 +1,12 @@
-// get the link element from the site
 const linkElement = document.querySelector('a[href*="https://maps.google.com/maps/"]');
 
-// get the href attribute of the link
 const href = linkElement.getAttribute('href');
 
-// extract the latitude and longitude from the URL
 const regex = /@(-?\d+\.\d+),(-?\d+\.\d+)/;
 const match = href.match(regex);
 const latitude = match[1];
 const longitude = match[2];
 
-// log the latitude, longitude, and link to the console
 console.log(`${latitude},${longitude}`);
 console.log(`${href}`);
 
